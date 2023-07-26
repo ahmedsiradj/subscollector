01f736463c9faf34abca47068f07d8e4cd38570e
unknown <amedsiradj9@gmail.com>
2023-07-26 09:41:15 +0100

    version 1.0 of subcollector

--- subscollector.py
+++ subscollector.py
@@ -1,59 +1,99 @@
-#!/usr/bin/env python
 
 import requests
 from bs4 import BeautifulSoup
 import os
 import re
-from datetime import date
+import json
+import threading
 import argparse
 from colored import fg,attr
 import sys
 
 
-def ouput(path,subdomains):
-    if path:
-        filename = os.path.basename(path)
-        dirname = os.path.dirname(path)
+
+class subscollector:
+    def __init__(self,domain,output,verbose):
+        self.domain = domain
+        self.subdomains = []
+        self.output = output
+        self.verbose = verbose
+
+        main_func = threading.Thread(target=self.run())
+        main_func.start()
+        main_func.join()
+
+
+
+        if(self.verbose):
+            self.display()
+
+        if(self.output):
+            self.write()
+
+    def run(self):
+
+        func1 = threading.Thread(target=self.rapiddns(self.domain))
+        func2 = threading.Thread(target=self.cth(self.domain))
+        func3 = threading.Thread(target=self.whoisxmlapi(self.domain))
+
+        func1.start()
+        func2.start()
+        func3.start()
+
+        func1.join()
+        func2.join()
+        func3.join()
+
+        self.subdomains = list(set(self.subdomains))
+
+    def rapiddns(self,domain):
+        page = requests.get(f"https://rapiddns.io/subdomain/{domain}#result")
+        soup = BeautifulSoup(page.content,'lxml')
+        list = soup.find_all(string=re.compile(f"([a-zA-Z]|[-])\.{domain}"))
+        self.subdomains.extend(list)
+
+
+
+    def cth(self,domain):
+        page = requests.get(f"https://crt.sh/?q={domain}")
+        soup = BeautifulSoup(page.content,"lxml")
+        list = soup.find_all(string=re.compile(f"([a-zA-Z]|[-])\.{domain}"))
+        self.subdomains.extend(list)
+
+
+
+    def whoisxmlapi(self,domain):
+        apikey = ""
+        if(not apikey):
+           return
+        response = requests.get(f"https://subdomains.whoisxmlapi.com/api/v1?apiKey={apikey}&domainName={domain}")
+        data = json.loads(response.content)
+        records = data["result"]["records"]
+        list = [x["domain"] for x in records]
+        self.subdomains.extend(list)
+
+
+
+    def write(self):
+        filename = os.path.basename(self.output)
+        dirname = os.path.dirname(self.output)
         if not os.path.exists(dirname):
-            print("[-] Invalid path: " + dirname)
+            print("[-] Invalid output: " + dirname)
             exit()
         os.chdir(dirname)
         file = open(filename,"w")
-        for sub in subdomains:
+        for sub in self.subdomains:
             file.write(sub + "\n")
         file.close()
 
-    else:
-        for sub in subdomains:
-            print(sub)
-
-def subdomains_enumeration(domain,path):
     
-    # collect subdomains from two sites
-    today = date.today()
-        
-    # Site 1    
-    page1 = requests.get(f"https://rapiddns.io/s/{domain}#result")
-    soup1 = BeautifulSoup(page1.content,'lxml')
-    list1 = soup1.find_all(string=re.compile(domain)) # that return a list contains subdomains from https://rapiddns.io/
     
-    # Site 2
-    page2 = requests.get(f"https://subdomainfinder.c99.nl/scans/{today}/{domain}")
-    soup2 = BeautifulSoup(page2.content,'html.parser')
-    data2 = soup2.find_all(class_="link sd")
-    list2 = []
-    for sub in data2:
-        list2.append(sub.string) # to get the text between tags <a>...</a>
     
-    # Site 3
-    page3 = requests.get(f"https://crt.sh/?q={domain}")
-    soup3 = BeautifulSoup(page3.content,"html.parser")
-    list3 = soup3.find_all(string=re.compile(domain))
+    def display(self):
+        for subdomain in self.subdomains:
+            print(subdomain)
 
-    subdomains = list(dict.fromkeys(list1 + list2 +list3))# to remove duplicate from the array
 
-    # Handel the ouput
-    ouput(path,subdomains)
 
     
 
@@ -66,10 +106,11 @@ if __name__ == "__main__":
                      ___/ / /_/ / /_/ (__  ) /__/ /_/ / / /  __/ /__/ /_/ /_/ / /    
                     /____/\__,_/_.___/____/\___/\____/_/_/\___/\___/\__/\____/_/     
 
+                                                                Version: 1.0
                                                                 Created By: @Siradj
                                                                 Github: github.com/ahmedsiradj
                     
-                    ''')
+                    {fg(15)}{attr(1)}''')
 
 
 
@@ -79,8 +120,10 @@ if __name__ == "__main__":
 
     parser.add_argument('-d', '--domain', required=True)
     parser.add_argument('-o', '--output', required=False, default=None)
+    parser.add_argument('-v','--verbose',action="store_true",help="To display the output",required=False)
     args = parser.parse_args()
     if sys.argv[2] == "-h" or sys.argv[2] == "--help":
         parser.print_help()
         exit()
-    subdomains_enumeration(args.domain,args.output)
+
+    subscollector(args.domain,args.output,args.verbose)
